0332c51ea4dd74890151c7de6fca9051326af2f0
cliffmieser <cliffmieser@gmail.com>
2023-02-14 18:42:05 -0500

    script refactoring

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,13 @@
+
 document.querySelector('form').addEventListener('submit', (e)=>{
     //get value of input
-    e.stopPropagation();
+    e.preventDefault();
     const result = document.createElement('p');
     let binary = document.querySelector("#digits").value; //gets value of input
     let decimal = parseInt(binary, 2);
     result.innerText = decimal;
     document.querySelector('body').appendChild(result);
 })
+
+
 
